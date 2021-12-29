3ec469bc343ead2ac4b28d612231104d49d94d20
GRIEGMAK <g.m.ak@yandex.ru>
2021-12-29 14:33:10 +0600

    правки дизайн продукт и категории

--- app/code/Icreative/ProductCatalogWidget/view/frontend/web/js/custom_appearance.js
+++ app/code/Icreative/ProductCatalogWidget/view/frontend/web/js/custom_appearance.js
@@ -1,16 +1,9 @@
 define(['jquery'], function($) {
-    document.querySelector(".add-to-card-button").addEventListener("mouseover", function () {
-        document.querySelector(".add-to-card-button").classList.toggle('action');
-        console.log('1add action')
+    $(".product-item-info").mouseenter(function () {
+        this.classList.toggle('focus-item');
     })
 
-
-    document.querySelector(".add-to-card-button").addEventListener("mouseout", function () {
-        document.querySelector(".add-to-card-button").classList.remove('action');
-        console.log('remove action')
-    })
-    $(".add-to-card-button").click(function (){
-        $(".add-to-card-button").addClass('action')
-        console.log('add action')
+    $(".product-item-info").mouseleave(function () {
+        this.classList.remove('focus-item');
     })
 });
